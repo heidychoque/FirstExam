694f46a4531652f10ce1e3000e0d90ddc4757e84
Paola Lizarraga <lizarragapk@gmail.com>
2018-05-01 14:15:27 -0400

    FE001 - Add generateIndex

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,4 +15,25 @@ module.exports = function (grunt) {
 	    }
 	  }
   	});
+
+  	grunt.registerTask('generateIndex', function() {//es para copiar un archivo de un lugar a otro
+		var config = grunt.file.readJSON('config.json');
+		grunt.file.copy('src/index.html','build/index.html',//copie 1.js a new...
+		{
+			process: function(files){//muestra lo que tiene
+				//console.log(content);
+				return grunt.template.process(files,
+					{
+						data: {
+							pageTitle: config.pageTitle,
+							styles: [config.buildFolder + "/styles.css"],
+							content: config.content,
+							scripts: [config.buildFolder + "/scripts.js"]
+						}
+					}
+					);
+			}
+
+		});
+	});
 };
