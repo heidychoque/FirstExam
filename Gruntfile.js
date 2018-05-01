d7861078b3733b4656ee543255c457b9cbc5e27e
marant <pensu15@gmail.com>
2018-05-01 15:00:33 -0400

    FE001 - concatJS and concatCSS added

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt){
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    var config = grunt.file.readJSON('config.json');
+
+    grunt.initConfig({
+        concat: {
+            JS: {
+              src: [config.srcFolder + '/js/one.js', config.srcFolder + '/js/two.js'],
+              dest: config.buildFolder+"/scripts.js"
+            },
+            CSS: {
+              src: [config.srcFolder + '/css/first.css', config.srcFolder + '/css/second.css', config.srcFolder + '/css/third.css'],
+              dest: config.buildFolder+"/styles.js"
+            }    
+        }
+    });
+
+    grunt.registerTask('concatJS', ['concat:JS']);
+        
+    grunt.registerTask('concatCSS', ['concat:CSS']);
+}
