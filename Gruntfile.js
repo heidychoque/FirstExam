bda5822a9c944949075ba7a52942677fd07734cb
Luz Elionor Cordova <elicg2012@hotmail.com>
2018-05-01 19:48:52 -0400

    FE001 - Concat de los .js realizado.

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt){
+    
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    
+    grunt.initConfig({
+        concat: {
+            JS: {
+                src: ['/src/js/one.js', '/src/js/two.js'],
+                dest: 'build/scripts.js'
+            }
+        }
+    });
+
+    grunt.registerTask('concatJS', 'concat:JS');
+
+}
