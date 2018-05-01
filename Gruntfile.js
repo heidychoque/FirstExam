6201ee637f71b7ad1f2a71a28c071d9cd43505cf
Sergio Oporto Tejerina <future_s_e_r_@hotmail.com>
2018-04-30 20:31:23 -0400

    FE001 - Added concatenation task for js scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,16 @@
 module.exports = function (grunt) {
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    
+    grunt.initConfig({
+        
+        config: grunt.file.readJSON('config.json'),
+        concat: {
+          JS: {
+            src: ['<%= config.srcFolder %>/js/one.js', '<%= config.srcFolder %>/js/two.js'],
+            dest: '<%= config.buildFolder %>/scripts.js'
+          }
+        }
+    });
     
 };
