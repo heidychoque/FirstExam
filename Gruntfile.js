a9933ce8f392db441acfa6b849af3f59ae89bb21
Eun Kyu Choi <choi2nkyu@gmail.com>
2018-05-01 13:33:37 -0400

    FE001 - created a function to create an index file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	var config = grunt.file.readJSON('config.json');
 	grunt.initConfig({
-		var config = grunt.file.readJSON('config.json');
 		concat: {
 			concatJS: {
 				src: [config.srcFolder + '/js/one.js', config.srcFolder + '/js/two.js'],
@@ -12,5 +12,18 @@ module.exports = function (grunt) {
 				dest: config.buildFolder + '/styles.css'
 			}
 		}
+	});
+	grunt.registerTask('generateIndex', function(){
+		grunt.file.copy('index.html', config.buildFolder + '/index.html', {
+			process: function(files){
+				return grunt.template.process(files,
+				{
+					data: { 
+						pageTitle: config.pageTitle,
+						pageContent: config.pageContent
+					}
+				});
+			}
+		});
 	});
 };
