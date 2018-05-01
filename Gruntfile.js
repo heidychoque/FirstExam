960fecc45b241c9d3b7bf6663b355519040989f6
Smitty <fabio_urca@hotmail.com>
2018-04-30 20:45:47 -0400

    FE001 - added concatCSS in file gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,13 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.initConfig({
         concat: {
-            js:{
+            concatJS:{
                 src: ['src/js/one.js','src/js/two.js'],
                 dest: 'build/scripts.js'
+            },
+            concatCSS:{
+                src:['src/css/first.css','src/css/second.css','src/css/third.css'],
+                dest: 'build/styles.css'
             }
         }
     });
