7a1770e13fb0f429c79db4226bb864e512b3dba4
Carlos Gamboa <carlo-x-17@hotmail.com>
2018-05-08 20:34:46 -0400

    FE001 - Fixed hard code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,21 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
 
+    var config = {};
+    config.pageTitle = grunt.option('title');
+    config.pageContent = grunt.option('content');
+    config.buildFolder = grunt.option('build');
+    config.srcFolder = grunt.option('src')
+
     grunt.initConfig({
         concat: {
             JS : {
-                src: ['src/js/one.js', 'src/js/two.js'],
-                dest:'build/scripts.js'
+                src: [config.srcFolder + '/js/one.js', config.srcFolder + '/js/two.js'],
+                dest: config.buildFolder + '/scripts.js'
             },
             CSS : {
-                src: ['src/css/first.css', 'src/css/second.css', 'src/css/third.css'],
-                dest:'build/styles.css'
+                src: [config.srcFolder +'/css/first.css', config.srcFolder +'/css/second.css', config.srcFolder + '/css/third.css'],
+                dest: config.buildFolder + '/styles.css'
             }
         }
     });
@@ -20,11 +26,7 @@ module.exports = function(grunt) {
 
     grunt.registerTask('generateIndex', function() {
         //var config = grunt.file.readJSON('config.json');
-        var config = {};
-        config.pageTitle = grunt.option('title');
-        config.pageContent = grunt.option('content');
-        config.buildFolder = grunt.option('build');
-        config.srcFolder = grunt.option('src')
+        
 
         grunt.file.copy(config.srcFolder + '/index.html', config.buildFolder + '/index.html' , {
             process: function (files) {
