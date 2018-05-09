c8eccd47c04d40ef9cbfaa8381b7b2b8a2a4da8f
Paola Lizarraga <lizarragapk@gmail.com>
2018-05-09 19:14:14 -0400

    FE001 - Configuration Jenkins

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,26 @@
 module.exports = function (grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	var config = grunt.file.readJSON('config.json');
+	//var config = grunt.file.readJSON('config.json');//esto ya no serviria
+	//var config = {};//objeto vacio
+	//config.pageContent = grunt.option('content');
+	var config = 
+	{
+		pageTitle: grunt.option('title'),
+		content: grunt.option('content'),
+		buildFolder: grunt.option('build'),
+		srcFolder: grunt.option('src')
+	};
 
 	grunt.initConfig({
 	  concat: {
 	    JS: {
 	      src: [config.srcFolder + 'js/one.js', config.srcFolder + 'js/two.js'],
-	      dest: config.srcFolder + 'scripts.js'
+	      dest: config.buildFolder + 'scripts.js'
 	    },
 	    CSS: {
-	      src: [config.srcFolder + 'css/first.css', config.srcFolder + 'css/second.css', 'src/css/third.css'],
-	      dest: config.srcFolder + 'styles.css'
+	      src: [config.srcFolder + 'css/first.css', config.srcFolder + 'css/second.css', config.srcFolder + 'src/css/third.css'],
+	      dest: config.buildFolder + 'styles.css'
 	    }
 	  }
   	});
@@ -20,8 +29,10 @@ grunt.registerTask('deployProd', //Create a multitask to execute all described a
 		['concat:JS', 'concat:CSS', 'generateIndex']);
 
 
-  	grunt.registerTask('generateIndex', function() {//es para copiar un archivo de un lugar a otro
-		var config = grunt.file.readJSON('config.json');
+  	grunt.registerTask('generateIndex', function() {
+  		//es para copiar un archivo de un lugar a otro
+  		console.log(config);
+		//var config = grunt.file.readJSON('config.json');
 		grunt.file.copy('src/index.html','build/index.html',//copie 1.js a new...
 		{
 			process: function(files){//muestra lo que tiene
