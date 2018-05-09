e13fa75d0a57c307ce0cf4479bb418e0e52af732
Sergio Oporto Tejerina <future_s_e_r_@hotmail.com>
2018-05-08 20:05:04 -0400

    JP001 - Added use of parameters for config variables

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,50 @@
 module.exports = function (grunt) {
-
-    grunt.loadNpmTasks('grunt-contrib-concat');
     
-    grunt.initConfig({   
-        config: grunt.file.readJSON('config.json'),
-        concat: {
-          JS: {
-            src: ['<%= config.srcFolder %>/js/one.js', '<%= config.srcFolder %>/js/two.js'],
-            dest: '<%= config.buildFolder %>/scripts.js'
-          },
-          CSS: {
-            src: ['<%= config.srcFolder %>/css/first.css', '<%= config.srcFolder %>/css/second.css'],
-            dest: '<%= config.buildFolder %>/styles.css'
-          }
+  var config = {
+    pageContent: grunt.option('content'),
+    pageTitle: grunt.option('title'),
+    srcFolder: grunt.option('src'),
+    buildFolder: grunt.option('build')
+  };
+  var configJSON = grunt.file.readJSON('config.json');
+  !config.pageContent && (config.pageContent = configJSON.pageContent);
+  !config.pageTitle && (config.pageTitle = configJSON.pageTitle);
+  !config.srcFolder && (config.srcFolder = configJSON.srcFolder);
+  !config.buildFolder && (config.buildFolder = configJSON.buildFolder);
+  
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  
+  grunt.initConfig({   
+      config: config,
+      concat: {
+        JS: {
+          src: ['<%= config.srcFolder %>/js/one.js', '<%= config.srcFolder %>/js/two.js'],
+          dest: '<%= config.buildFolder %>/scripts.js'
+        },
+        CSS: {
+          src: ['<%= config.srcFolder %>/css/first.css', '<%= config.srcFolder %>/css/second.css'],
+          dest: '<%= config.buildFolder %>/styles.css'
+        }
+      }
+  });
+
+  grunt.registerTask('concatJS', 'concat:JS');
+  grunt.registerTask('concatCSS', 'concat:CSS');
+  
+  grunt.registerTask('generateIndex', function(){
+    grunt.file.copy(config.srcFolder+'/index.html', config.buildFolder+'/index.html', {
+        process: function(files){
+            return grunt.template.process(files, {
+                data: {
+                  stylesFile: 'styles.css',
+                  scriptsFile: 'scripts.js',
+                  pageTitle: config.pageTitle,
+                  pageContent: config.pageContent
+                }
+            });
         }
     });
+  });
 
-    grunt.registerTask('concatJS', 'concat:JS');
-    grunt.registerTask('concatCSS', 'concat:CSS');
-    
-    grunt.registerTask('generateIndex', function(){
-      var config = grunt.file.readJSON('config.json');
-      grunt.file.copy(config.srcFolder+'/index.html', config.buildFolder+'/index.html', {
-          process: function(files){
-              return grunt.template.process(files, {
-                  data: {
-                    stylesFile: 'styles.css',
-                    scriptsFile: 'scripts.js',
-                    pageTitle: config.pageTitle,
-                    pageContent: config.pageContent
-                  }
-              });
-          }
-      });
-    });
-
-    grunt.registerTask('build', ['generateIndex', 'concat'])
+  grunt.registerTask('build', ['generateIndex', 'concat'])
 }; 
